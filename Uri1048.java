6cc7e72719997789bb3aca8d41f6e3005eee2c3e
Biancaandradee <bi_andradehandan@hotmail.com>
2022-02-03 19:23:38 -0300

    Uri1048

--- Uri1048.java
+++ Uri1048.java
@@ -5,25 +5,26 @@ public class Uri1048 {
 
     Scanner keyboard = new Scanner(System.in);
 
-    double salary, bonus, perc, newSalary;
+    double salary, bonus, newSalary, perc;
     salary = keyboard.nextDouble();
 
     if (salary > 0 && salary <= 400.00) {
-        perc = 15;
+        perc = 0.15;
     } else if (salary > 400.00 && salary <= 800.00){
-        perc = 12;
+        perc = 0.12;
     } else if (salary > 800.00 && salary <= 1200.00) {
-        perc = 10;
+        perc = 0.10;
     } else if (salary > 1200.00 && salary <= 2000.00) {
-        perc = 7;
+        perc = 0.07;
     } else {
-        perc = 4;
+        perc = 0.04;
     }
 
     bonus = salary * perc;
     newSalary = salary + bonus;
-        
-    System.out.printf("Novo salario: %.2f\n Reajuste ganho: %.2f\n Em percentual: %d '%'\n", newSalary, bonus, perc);
+    //perc = perc * 100;
+
+    System.out.printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %%\n", newSalary, bonus, (int)(perc*100));
 
     keyboard.close();
 

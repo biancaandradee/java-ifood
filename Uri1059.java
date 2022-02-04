552386cec339c6d4ca9f3a0d5cf8a1335354f9a1
Biancaandradee <bi_andradehandan@hotmail.com>
2022-02-04 14:22:32 -0300

    Uri1059

---
+++ Uri1059.java
@@ -0,0 +1,14 @@
+public class Uri1059 {
+    public static void main(String args[]) {
+
+        // for (int num = 1; num <= 100; num++) {
+        //     if (num % 2 == 0) {
+        //         System.out.print(num);
+        //     }
+        // }
+
+        for (int num = 2; num <= 100; num += 2) {
+            System.out.println(num);
+        }
+    }
+}
